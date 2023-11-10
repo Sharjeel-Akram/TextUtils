a6e438316962856845519795c7f6086392b39bf7
Sharjeel-Akram <sharjeel.akram0200@gmail.com>
2023-11-11 04:49:21 +0500

    Design Done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -47,14 +47,14 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
-    const [text, setText] = useState("Enter Text Here");
+    const [text, setText] = useState("");
 
     return (
         <>
             <div className= "container" style = {{color: props.mode === 'dark' ? 'white': 'black'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" style = {{backgroundColor: props.mode === 'dark' ? 'grey': 'white', color: props.mode === 'dark' ? 'white': 'black' }}value={text} onChange={handleChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" style = {{backgroundColor: props.mode === 'dark' ? 'grey': 'white', color: props.mode === 'dark' ? 'white': 'black' }} value={text} onChange={handleChange} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Upper Case</button>
                 <button className="btn btn-primary mx-1 my-1" onClick={handleloClick}>Convert to Lower Case</button>
